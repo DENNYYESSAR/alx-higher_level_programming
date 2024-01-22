85d9bde5c49bd4f9e9887aa8a1bbfdcd30567e7a
DENNYYESSAR <dennisokeri5@gmail.com>
2024-01-22 06:22:31 -0800

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def safe_print_integer_err(value):
     """Prints an integer followed by a new line.
 
@@ -13,7 +14,7 @@ def safe_print_integer_err(value):
     """
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return False
+        return (False)
