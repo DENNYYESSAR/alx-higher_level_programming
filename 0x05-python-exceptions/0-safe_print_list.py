e1e88b8829c900e62a557267560dbd9872eba34c
DENNYYESSAR <dennisokeri5@gmail.com>
2024-01-22 04:16:09 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=None, x=0):
+    """Print x elements of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The real number of elements printed.
+    """
+    if my_list is None:
+        my_list = []
+
+    printed_elements = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            printed_elements += 1
+    except IndexError:
+        pass
+
+    print()
+    return printed_elements
