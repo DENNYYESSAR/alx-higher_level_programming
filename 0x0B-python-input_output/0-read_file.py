f5a494fcbe3c28fb2ca51cc447ac0973d4e04af6
DENNYYESSAR <dennisokeri5@gmail.com>
2024-02-06 09:00:51 -0800

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function to read a text file (UTF8) and print its content to stdout.
+"""
+
+
+def read_file(filename=""):
+    """Reads the content of the specified file and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as file:
+        file_content = file.read()
+        print(file_content)
