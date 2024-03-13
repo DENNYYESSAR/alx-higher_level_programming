d20741da0fc37d23fbf9969cecd2ea8a30538491
DENNYYESSAR <dennisokeri5@gmail.com>
2024-03-13 08:55:14 -0700

    Always a name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- creates the table force_name on your MySQL server
+-- Create the table force_name if it doesn't exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
