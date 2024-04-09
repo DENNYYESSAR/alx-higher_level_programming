8800753a4bd71556efe3a4cd0c558499c8e3eb38
DENNYYESSAR <dennisokeri5@gmail.com>
2024-04-09 15:19:16 +0300

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
