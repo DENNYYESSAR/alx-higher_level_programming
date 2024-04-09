717a38ee13074f638dfd8f241b60513d55bb3db0
DENNYYESSAR <dennisokeri5@gmail.com>
2024-04-09 15:57:59 +0300

    Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  return function convertToBase (number) {
-    if (number === 0) {
-      return '';
-    } else {
-      return convertToBase(Math.floor(number / base)) + (number % base).toString(base).toUpperCase();
-    }
+  return function (num) {
+    return num.toString(base);
   };
 };
