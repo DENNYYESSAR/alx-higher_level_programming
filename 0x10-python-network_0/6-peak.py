21a95e1a6a68f5daff3f5b47ce1c5f5a290811ea
DENNYYESSAR <dennisokeri5@gmail.com>
2024-04-25 04:46:33 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
+"""Algorithms for list of integers."""
+
 
 def find_peak(list_of_integers):
-    """
-    Find a peak in a list of unsorted integers.
-
-    Args:
-    - list_of_integers: A list of unsorted integers.
-
-    Returns:
-    - A peak from the list.
-    """
-    if not list_of_integers:
-        return None
-
-    left = 0
-    right = len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    return list_of_integers[left]
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
