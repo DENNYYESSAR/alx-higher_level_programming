0bbdff4aa955447a3fc153522176677ab51e40de
DENNYYESSAR <dennisokeri5@gmail.com>
2024-04-26 04:25:30 -0700

    My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Takes your GitHub credentials (username and personal access token)
+and uses the GitHub API to display your id.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_info = response.json()
+        print("Your GitHub ID is:", user_info['id'])
+    else:
+        print("Failed to fetch user information. HTTP Status Code:",
+              response.status_code)
