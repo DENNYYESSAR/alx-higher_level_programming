1af309991dd03ea3381a5ac5266355d373c4f9c7
DENNYYESSAR <dennisokeri5@gmail.com>
2024-04-26 04:48:50 -0700

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     url = 'https://api.github.com/user'
 
     response = requests.get(url, auth=(username, password))
-    
+
     try:
         user_info = response.json()
         print(user_info.get('id'))
