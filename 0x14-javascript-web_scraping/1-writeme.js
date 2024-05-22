4fe2fc7a436dd4530b8b6d5e699db450d97ab543
DENNYYESSAR <dennisokeri5@gmail.com>
2024-05-22 15:23:30 +0300

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
 
-if (!filePath || !stringToWrite) {
-  console.error('Usage: ./write_file.js <file_path> <string_to_write>');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  }
-});
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  
