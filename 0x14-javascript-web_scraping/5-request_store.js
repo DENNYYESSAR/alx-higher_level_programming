ac6596656b18a77b176b593bd321bea9c4898b6b
DENNYYESSAR <dennisokeri5@gmail.com>
2024-05-22 15:03:24 +0300

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,32 +1,17 @@
 #!/usr/bin/node
+// lorem ipsum
 
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-if (!url || !filePath) {
-  console.error('Usage: ./script.js <URL> <file_path>');
-  process.exit(1);
-}
-
-request.get(url, (error, response, body) => {
+request.get(process.argv[2], (error, response, body) => {
   if (error) {
-    console.error(error);
-    process.exit(1);
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch URL: ${response.statusCode}`);
-    process.exit(1);
-  }
-
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (err) {
-      console.error(`Failed to write to file: ${err.message}`);
-      process.exit(1);
-    }
-    console.log(`Successfully saved response to ${filePath}`);
-  });
 });
